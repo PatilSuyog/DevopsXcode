d8d824f4f881f3c780865313d7b35eea1f4efa6d
Suyog Patil <suyogp@winjit.com>
2019-06-01 12:25:40 +0530

    New project and initial commit

---
+++ DevopsTraining/DevopsTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevopsTraining
+//
+//  Created by Winjit-Suyog on 01/06/19.
+//  Copyright © 2019 Winjit Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
